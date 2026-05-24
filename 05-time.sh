ccef6d088393b12ce128d338a7a64667757e01fa
sureshreddybmr <sureshreddybmr@gmail.com>
2026-05-24 20:48:42 +0530

    Script created

--- 05-time.sh
+++ 05-time.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
-echo "Current date and time is: $Timestamp"
+starttime=$(date +%s)
+echo "This is the time script"
+endtime=$(date +%s)
+totaltime=$((endtime - starttime))
+echo "Total time taken to execute the script is: $totaltime seconds"
