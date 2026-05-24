65b8053d2554266ad777af8d10cb632b76ff1613
sureshreddybmr <sureshreddybmr@gmail.com>
2026-05-24 20:29:19 +0530

    Script created

---
+++ 03-varibales.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1="$1"
+person2="$2"
+echo "$person1:Welcome to fevops"
+echo "$person2:Welcome to fevops"
+echo $person1:This is the conversions script"
+echo $persion2:This script will cover the conversions in bash"
+echo "Let's start with the first conversion"
+echo "Converting a string to uppercase"
