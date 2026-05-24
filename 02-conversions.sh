9f962e27792afa8525996afc741febb46b8ccee3
sureshreddybmr <sureshreddybmr@gmail.com>
2026-05-24 20:15:48 +0530

    Script created

---
+++ 02-conversions.sh
@@ -0,0 +1,6 @@
+#!/bin/basah
+echo "Welcome to fevops"
+echo "This is the conversions script"
+echo "This script will cover the conversions in bash"
+echo "Let's start with the first conversion"
+echo "Converting a string to uppercase"
